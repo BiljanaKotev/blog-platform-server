b78d339adbbd097104dcdfcf076644daba9c5474
Biljana Kotevska <biljanakotev@gmail.com>
2023-09-30 23:36:13 +0200

    commit

---
+++ models/Post.model.js
@@ -0,0 +1,29 @@
+const { Schema, model } = require('mongoose');
+
+const postSchema = new Schema(
+  {
+    coverImg: {
+      type: String,
+      required: [true, 'Image is required'],
+    },
+    title: {
+      type: String,
+      required: [true, 'Title is required.'],
+    },
+    location: {
+      type: String,
+      required: [true, 'Location is required.'],
+    },
+    description: {
+      type: String,
+      required: [true, 'Description is required.'],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Post = model('Post', postSchema);
+
+module.exports = Post;
