8e7584266413d866f8d20827585946d4afea1032
Biljana Kotevska <biljanakotev@gmail.com>
2023-10-03 09:45:04 +0200

    added new blog-post route

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -4,13 +4,24 @@ const mongoose = require('mongoose');
 const Post = require('../models/Post.model');
 
 router.get('/dashboard', (req, res, next) => {
-  console.log('Dashboard accessed');
   if (!req.user) {
     return res.status(400).send('Unauthorized');
   }
   Post.find({ userId: req.user._id })
-    .then((posts) => {
-      res.json(posts);
+    .then((dashboard) => {
+      res.json(dashboard);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.post('/blog-post', (req, res, next) => {
+  console.log('accessed new post');
+  const { coverImg, title, location, description } = req.body;
+  Post.create({ coverImg, title, location, description })
+    .then((post) => {
+      res.json(post);
     })
     .catch((err) => {
       next(err);
