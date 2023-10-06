ba23f43d2cf5c080bdefbde740dc2fe51b40c5f6
Biljana Kotevska <biljanakotev@gmail.com>
2023-10-06 15:35:50 +0200

    add dynamic route for userposts

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -3,12 +3,13 @@ const router = express.Router();
 const Post = require('../models/Post.model');
 const fileUploader = require('../config/cloudinary.config');
 const { isAuthenticated } = require('../middleware/jwt.middleware');
+const User = require('../models/User.model');
 
 router.get('/dashboard', (req, res, next) => {
   if (!req.payload) {
     return res.status(400).send('Unauthorized');
   }
-  Post.find({ userId: req.payload._id })
+  Post.find({ author: req.payload._id })
     .then((dashboard) => {
       res.json(dashboard);
     })
@@ -18,9 +19,10 @@ router.get('/dashboard', (req, res, next) => {
 });
 
 router.post('/create-post', (req, res, next) => {
-  const { coverImg, title, location, content } = req.body;
-  Post.create({ coverImg, title, location, content })
+  const { coverImg, title, location, content, author } = req.body;
+  Post.create({ coverImg, title, location, content, author })
     .then((newPost) => {
+      console.log('newpost', newPost);
       res.status(200).json(newPost);
     })
     .catch((err) => {
@@ -41,11 +43,15 @@ router.get('/user-posts', (req, res, next) => {
 });
 
 router.get('/blog-feed', (req, res, next) => {
-  console.log('works');
-
   Post.find()
     .populate('author')
     .then((postsFromDB) => {
+      postsFromDB.forEach((post) => {
+        if (!post.author) {
+          console.log('Missing author for post with _id:', post._id);
+        }
+      });
+
       res.status(200).json(postsFromDB);
     })
     .catch((err) => {
@@ -70,7 +76,7 @@ router.get('/blog-feed/:id', (req, res, next) => {
     });
 });
 
-router.get('/dashboard/blog-post/:id', (req, res, next) => {
+router.get('/user-posts/:id', (req, res, next) => {
   const id = req.params.id;
 
   Post.findById(id)
@@ -95,6 +101,26 @@ router.post('/upload', fileUploader.single('imgUrl'), (req, res, next) => {
   const responseObject = { fileUrl: req.file.path };
   console.log('Server side fileUrl', responseObject);
   res.json({ fileUrl: req.file.path });
+});
+
+router.post('/update-user-profile-pic', (req, res) => {
+  const { userId, profilePicUrl } = req.body;
+
+  User.findById(userId)
+    .then((user) => {
+      if (!user) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+
+      user.profilePicUrl = profilePicUrl;
+      return user.save();
+    })
+    .then(() => {
+      res.status(200).json({ message: 'Profile picture updated successfully' });
+    })
+    .catch((error) => {
+      res.status(500).json({ message: 'Error updating profile picture' });
+    });
 });
 
 module.exports = router;
