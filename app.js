f296e081645b1de076c822ec9351c81d1e788d85
Biljana Kotevska <biljanakotev@gmail.com>
2023-10-18 17:27:15 +0200

    Update app.js CORS configuration

--- app.js
+++ app.js
@@ -10,6 +10,28 @@ const cors = require('cors');
 
 // 👇 Configure CORS
 
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://travelhub-blog-platform.netlify.app',
+  // ... any other origins you might use
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true); // allow requests with no origin
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg = 'The CORS policy for this site does not allow access from the specified origin.';
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+    allowedHeaders: ['Content-Type', 'Authorization', 'Cache-Control'], // <-- Add 'Cache-Control' here
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // specify the methods you want to allow
+    credentials: true,
+  })
+);
+
 // const whitelist = ['http://localhost:3000', 'https://travelhub-blog-platform.netlify.app'];
 // const corsOptions = {
 //   origin: function (origin, callback) {
@@ -24,12 +46,12 @@ const cors = require('cors');
 
 // app.use(cors(corsOptions));
 
-app.use(
-  cors({
-    origin: 'https://travelhub-blog-platform.netlify.app',
-    allowedHeaders: ['Content-Type', 'Authorization'],
-  })
-);
+// app.use(
+//   cors({
+//     origin: 'https://travelhub-blog-platform.netlify.app',
+//     allowedHeaders: ['Content-Type', 'Authorization'],
+//   })
+// );
 
 require('./config')(app);
 
